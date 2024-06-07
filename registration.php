e72c569f181e94b29e7628b5f1bcf7b5199da0f5
Vincent Marmiesse <vincent.marmiesse@ph2m.com>
2024-06-07 09:43:43 +0200

    Init module

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Copyright © PH2M SARL. All rights reserved.
+* See LICENSE for license details.
+*/
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PH2M_Elasticsearch',
+    __DIR__
+);
